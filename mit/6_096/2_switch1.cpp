4be792671043ceb2bccdcad6c0c3a60a60b2bf3f
Joshua Sucsy <jsu@falconfone.com>
2012-12-07 17:35:52 +0000

    Periodic commit

---
+++ mit/6_096/2_switch1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int x= 6;
+
+  switch(x){
+  case 1:
+	cout <<"x is 1\n";
+	break;
+  case 2:
+  case 3:
+	cout << "x is 2 or 3\n";
+  default:
+	cout << "x is not 1, 2, or 3\n";
+  }
+  return 0;
+}
