c6b8c1235fe6a286a1274b1c7fea67e47b3a277a
Joshua Sucsy <jsu@falconfone.com>
2012-12-19 13:52:02 +0000

    bitwise c work - incomplete

--- c/bitwise.c
+++ c/bitwise.c
@@ -1,3 +1,39 @@
 #include <stdio.h>
 
-int 
+int odd_even(int x){
+  if ((x & 1) == 0){
+	printf ("x is even\n");
+	}
+	else {
+	  printf ("x is odd\n");
+	}
+	return 0;
+}
+
+int main() {
+  int x;
+  int selection;
+
+  do{
+  printf("\n0 - Exit ");
+  printf("\n1 - OddEven: ");
+  printf("\nSelect option: ");
+  scanf("%d", &selection);
+  printf("\nEnter parameter: ");
+  scanf("%d", &x);
+
+  switch (selection)
+  {  
+	case 0:
+	  break;
+    case 1:
+	odd_even(1);
+	break;
+  default:
+	break;
+	}
+  }
+  while (selection != 0);
+
+  return 0;	
+}
